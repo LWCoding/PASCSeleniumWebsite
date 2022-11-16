2cc64085692f85dde92eeaa1d155a69f8c5e9379
LWCoding <lwangprivate@gmail.com>
2022-11-16 09:16:27 -0600

    Modified authenticator code

--- src/components/Authenticator.js
+++ src/components/Authenticator.js
@@ -2,24 +2,12 @@
 import React, { Component } from 'react'
 // CSS
 import "./Authenticator.css";
-// GOOGLE LOGIN
-import GoogleLogin from 'react-google-login';
-
-const responseGoogle = (response) => {
-  console.log(response);
-}
 
 export class Authenticator extends Component {
     render() {
         return (
             <div className="auth-box">
-                <GoogleLogin
-                    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                    buttonText="Login"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                />
+                Hi Login please
             </div>
         )
     }
